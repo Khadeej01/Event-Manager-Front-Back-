1221540ff1c78a2b1e109250f43c88481e320bb9
khadijaDev <khadijaoukhlift@gmail.com>
2025-05-30 17:49:44 +0200

    Fixed footer email rendering with mailto link and verified navbar/footer

---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [NavbarComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  exports: [NavbarComponent, FooterComponent]
+})
+export class SharedModule {}
