03b6bcc26e214e93d93cccde45080d7d7ed496cf
khadijaDev <khadijaoukhlift@gmail.com>
2025-06-01 00:43:36 +0200

    Navbar & footer done

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
-
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'; // Add this import
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,7 +10,7 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [NavbarComponent, FooterComponent],
   imports: [
-    CommonModule,
+    CommonModule, // Add this to imports
     RouterModule,
     MatToolbarModule,
     MatButtonModule,
